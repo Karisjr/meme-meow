3df7b8dda76015685159ec3f70df0d280b7274c3
Karis <karisjreimers@outlook.com>
2023-03-02 12:50:07 +0000

    initial

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="./cat.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme-meow Generator</h2>
+            <h4 className="header--project">Purrr-fect!</h4>
+        </header>
+    )
+}
